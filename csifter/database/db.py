ce1d57bb68a69e8dab789baea67b135765cfff77
Angus-C-git <anguscornall@gmail.com>
2021-09-05 18:51:08 +1000

    UPDATED :: badapi detection

--- csifter/database/db.py
+++ csifter/database/db.py
@@ -31,16 +31,88 @@ signatures = [
 
     {
         "name": "Unsafe memset Usage",
-        "description": "The memset is used to populate a memory region stipulated by the programmer with a supplied value. " + 
-                       "When the argument order is **confused** and the value to set, the second argument, is provided as the 3rd " + 
-                       "argument, and the size as the 2nd argument, the memory will not be cleared/set as expected. This typically results in" + 
+        "description": "memset is used to populate a memory region stipulated by the programmer with a supplied value. "                        + 
+                       "When the argument order is **confused** and the value to set, the second argument, is provided as the 3rd "             + 
+                       "argument, and the size as the 2nd argument, the memory will not be cleared/set as expected. This typically results in"  + 
                        "leaving behind stale data that can lead to unexpected behaviour.\n\n" +
                        "Correct usage of the function to zero out a memory region is: \n\n" +
-                       "`memset(address, 0, size);`",
-    
+                       "`   memset(address, 0, size);`",    
         "rules": [
             "memset[ ]*\([^\,]+\,[^\,]+\,[^\,|1-9]+\);",
         ]
-    }
 
-]   
+    },
+
+    {
+        # Currently acts as a warning of potential risk
+        "name": "Potential Buffer Overflow",
+        "description": "strcpy is used to copy a non-static source string to a destination buffer. If the destination buffer has "             +    
+                        "inadequate space allocated to store the incoming source string or the incoming data is improperly sanitised a "       +
+                        "buffer overflow could occur leading to undefined behaviour. It is a better idea to user `strncpy` which allows for "  +
+                        "the specification of a limit of bytes to read from source.",
+        
+        "rules": [
+            "strcpy[ ]*\([^\,]+\,[^\,\'\"]+\);",
+        ]
+    },
+
+    {
+        "name": "Potentially Unsafe strncpy",
+        "description": "The strncpy function copies at most `size_t n` bytes from the source string to the destination buffer. If a terminating "+
+                        "null byte **does not** lie within these **n bytes** the copied string will not be null terminated.\n\n If the copied "  +
+                        "string is then blindly used with another function that reads/writes until a null byte the function will continue "      +
+                        "reading/writing until a null byte is encountered leading to unexpected behaviour and potential security concerns.",
+
+        "rules": [
+            "strncpy[ ]*\([^\,]+\,[^\,\'\"]+\,[^\,]+\);",
+        ]
+    }, 
+    
+    {
+        "name": "Potential Buffer Overflow",
+        "description": "strcat is used to copy a non-static source string to a destination buffer. If the destination buffer has "             +    
+                        "inadequate space allocated to store the incoming source string or the incoming data is improperly sanitised a "       +
+                        "buffer overflow could occur leading to undefined behaviour. It is a better idea to user `strncat` which allows for "  +
+                        "the specification of a limit of bytes to read from source.",
+
+        "rules": [
+            "strcat[ ]*\([^\,]+\,[^\,\'\"]+\);",
+        ]
+    },
+
+    {
+        "name": "Potential Out of Bounds Read",
+        "description": "atoi is used to convert a potentially non-static argument to an integer which is then used directly as an array index. " +
+                        "If the supplied argument is user controlled it is likely that an attacker can leverage this to cause and out of bounds "+
+                        "read leading to unexpected behaviour and security concerns.",
+        
+        "rules": [
+            "\[atoi[ ]*\([^\,\'\"]+\)\];",
+        ]
+    },
+
+
+    {
+        "name": "Buffer Overflow",
+        "description": "gets is a **deprecated** function due to its behaviour to continue reading from stdin until a newline, or EOF, is encountered. "+
+                        "This behaviour can be exploited to cause a buffer overflow. Instead use `fgets` which allows for the specification of a "  +
+                        "limit of bytes to read from stdin.",
+
+        "rules": [
+            "gets[ ]*\([^\,]+\);",
+        ]
+    },
+
+
+    {
+        "name": "Potentially Bad sizeof for Memory Allocation",
+        "description": "sizeof is potentially called on a pointer instead of the datastructure being allocated. This means that the size of the pointer " +
+                        "will be returned resulting in less memory being allocated/set than likely intended.",
+
+        # TODO -> fix missed multiline syntax varianets
+        "rules": [
+            "malloc[ ]*\([ ]*sizeof[ ]*\([^\,\*]+\);",
+            "realloc[ ]*\([^\,]+\,[ ]*sizeof[ ]*\([^\,\*]+\);",
+        ]
+    }
+]
--- tests/badapis.c
+++ tests/badapis.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*
 Test detection of general bad
@@ -17,23 +18,43 @@ _____________________________
     + strcpy
     + strncpy
     + strcat
-    + strncat
-    + memcpy
+    + ?memcpy?
     + memset
+    + atoi
 
 */
+
+
+typedef struct testcase {
+    char *str;
+} testcase_t;
+
 
 void
 bad_api_usage(void)
 {
     char *str = "Hello";
     char *str2 = "World";
-    char *str3 = "!";
+
+    char buf[10];
+    char *dummy_usr_input = "overflowwwwwwwwwwwwwwwwwwwwww";
+
+    testcase_t *testcase = 0;
 
     /* UNSAFE */
 
     memset(str, 0x100, 0);
-    
+    strcpy(buf, dummy_usr_input);
+    strncpy(buf, dummy_usr_input, 42);
+    strcat(str, str2);
+    // memcpy(buf, dummy_usr_input, 42);
+    char character = str[atoi(dummy_usr_input)];
+    gets(buf);
+
+    testcase = (testcase_t *) malloc(sizeof(testcase));
+    testcase = (testcase_t *) realloc(testcase, sizeof(testcase));
+
+
     /* ALT SYNTAX */
     
     memset(
@@ -42,8 +63,43 @@ bad_api_usage(void)
             0
         );
 
+    strcpy(
+            buf,
+            dummy_usr_input
+        );
 
+    strncpy(
+            buf,
+            dummy_usr_input,
+            42
+        );
+    
+    strcat(
+            str,
+            str2
+        );
+
+    gets(
+            buf 
+        );
+
+    testcase = (testcase_t *) malloc(  
+                                        sizeof(testcase));
+    
+    testcase = (testcase_t *) realloc(
+                                        testcase, 
+                                        sizeof(testcase)
+                                    );
     /* SAFE */
     
     memset(str, 0, 1000);
+    // safer
+    strcpy(buf, "staticarg");
+    strncpy(buf, "staticarg", 10);
+    strcat(str, "staticarg");
+    testcase = (testcase_t *) malloc(sizeof(*testcase));
+    testcase = (testcase_t *) realloc(testcase, sizeof(*testcase));
 }
+
+
+int main(){return 0;}
