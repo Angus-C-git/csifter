9ba0271e61254ecb48126ed5a255a1f347024285
Angus-C-git <anguscornall@gmail.com>
2021-09-07 15:22:36 +1000

    UPDATE :: gcc wrapper

--- csifter/util/gcc.py
+++ csifter/util/gcc.py
@@ -1,19 +1,21 @@
 import subprocess
 
-'''
-‣ Run GCC to trigger its 
-  source checks
-‣ GCC will chuck a spaz if it
-  picks up very trivial vulns 
-‣ GCC has a surprisingly large amount of 
-  source checks built into it
-'''
+
 def run_compiler_checks(file):
-    print(f'[>->] Running GCC against {file}')
+    """
+    ‣ Run GCC to trigger its 
+      source checks
+    ‣ GCC will chuck an error if it
+      picks up very trivial vulnerabilities 
+    ‣ GCC has a surprisingly large amount of 
+      source checks built into it
+    """
+
+    print(f'[>->] Running GCC compiler checks against {file}')
     try:
         gcc_res = subprocess.run([
             'gcc',
-            '-Wall', 
+             # '-Wall',                       # drop wall to avoid unused variable warnings
             '-Wformat-security',              # Format strings 
             '-Wduplicated-cond',              # Logic bugs
             '-Wfloat-equal',                  # Comparison bugs (floats)
@@ -29,6 +31,6 @@ def run_compiler_checks(file):
         ])
     except Exception as e:
         print(f'''
-        {Fore.LIGHTRED_EX} Is GCC installed, or perhaps the file
+        {Fore.LIGHTRED_EX} Check GCC installed, or perhaps the file
             cannot be compiled by GCC and all is well
         ''')
